ce8604e06674dbc59692c17363910331876622de
MPBunce <mattb1299@gmail.com>
2023-09-30 15:55:47 -0400

    backend setup

--- backend/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def getDate(request):
-
-    person = {'name': 'Matt'}
-
-    return Response(person)
