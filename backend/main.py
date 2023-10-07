ce2e78710c477806c0bfa846aae9301ed326e54e
MPBunce <mattb1299@gmail.com>
2023-10-07 17:11:20 -0400

    status code

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/health-check")
+@app.get("/health-check", status_code=200)
 async def root():
     return {"message": "Hello World Health Check"}
 
