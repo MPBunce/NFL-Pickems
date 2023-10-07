7bd6b31b9176d834dac07cb96938f0382426ef1b
MPBunce <mattb1299@gmail.com>
2023-10-07 15:43:36 -0400

    nice

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/healthcheck")
+async def root():
+    return {"message": "Hello World Health Check"}
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Healthy"}
