727a9a4d5954bfce7d36102d985cdf07f1fcd082
MPBunce <mattb1299@gmail.com>
2023-10-09 15:11:59 -0400

    getting team data

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,39 @@
-from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from dotenv import dotenv_values
+from fastapi import FastAPI, status, Depends, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List, Annotated
+from models import Users, standings_2023
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get('/api/season_2023/')
+async def root( db: db_dependency):
+    try:
+        # Assuming you have a model called YourModel
+        results = db.query(standings_2023).all()
+        return results
+    except Exception as e:
+        return JSONResponse({"error": str(e)})
 
 
 @app.get("/api")
 async def root():
-    return {"message": "Hello World Health Check"}
+    return JSONResponse(content={"message": "Hello World Health Check"})
 
 @app.get("/")
 async def root():
-    return {"message": "Healthy"}
+    return JSONResponse(content={"message": "Healthy"})
---
+++ backend/models.py
@@ -0,0 +1,19 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class Users(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
+
+class standings_2023(Base):
+    __tablename__ = 'nfl-season-2023'
+
+    id = Column(Integer, primary_key=True, index=True)
+    team_name = Column(String)
+    wins = Column(Integer)
+    losses = Column(Integer)
+    ties =Column(Integer)
+    team_division = Column(String)
