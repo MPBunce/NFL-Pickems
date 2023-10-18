3704622ef59a126df63553f68cf37c3f2712b806
MPBunce <mattb1299@gmail.com>
2023-10-18 19:26:45 -0400

    ruting 4

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
   import Navbar from './components/Navbar.vue';
-  import router from './router/index.js'
+
 </script>
 
 
 <template>
 
   <Navbar/>
-
-  <div class="bg-indigo-500 text-black">
-    App.Vue
-  </div>
 
   <router-view></router-view>
 
