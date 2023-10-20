a555b1324c1573d123a961bb2f9344ea9a672b43
Matthew Bunce <mbunce@uoguelph.ca>
2023-10-19 22:03:40 -0400

    localhost

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,8 @@ app = FastAPI()
 
 origins = [
     "https://d3nsp1xpq5b06f.cloudfront.net",
-    "http://192.168.2.18:5173"
+    "http://192.168.2.18:5173",
+    "http://localhost:5173"
 ]
 
 app.add_middleware(
