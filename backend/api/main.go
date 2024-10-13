6e05909a3bda7e6fd39c587ebf34018e2a83c744
Matthew Bunce <matthew-bunce@outlook.com>
2024-10-13 15:11:19 -0400

    gitignore

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,7 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   string
 }
 
 type application struct {
@@ -24,25 +30,67 @@ type application struct {
 func main() {
 
 	var cfg config
-	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	// app := &application{
-	// 	config: cfg,
-	// 	logger: logger,
-	// }
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatalf("Error loading .env file: %s", envErr)
+	}
+	cfg.db = os.Getenv("MONGO_URI")
+
+	fmt.Print(cfg.db)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
 
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		// Handler:      app.routes(),
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+
+	_, b := openDB(cfg)
+	if b != nil {
+		fmt.Printf("?")
+	}
+
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
+
+}
+
+func openDB(cfg config) (*mongo.Client, error) {
+
+	opts := options.Client().ApplyURI(cfg.db)
+
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to MongoDB!")
+
+	return client, nil
 }
